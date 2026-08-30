0b7d8fba0abeb121b1bca550697bd505f7c3431d
Hirun Dilshan Wickramasinghe <hirundwickramasinghe@gmail.com>
2026-08-30 19:14:01 +0530

    ads

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { SnackbarProvider } from "notistack";
 import App from "./App";
-import NavBar from "./components/NavBar";
+import AdBar from "./components/AdBar";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <SnackbarProvider maxSnack={3}>
             <App />
+            <AdBar position="bottom" />
         </SnackbarProvider>
     </StrictMode>
 );
